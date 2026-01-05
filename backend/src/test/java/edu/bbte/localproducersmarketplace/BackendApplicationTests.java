889e37f77ae13eca4f2a34c3435d8aa9a4667212
Henrik-13 <balint.henrik13@gmail.com>
2026-01-05 12:44:47 +0200

    init spring project

---
+++ backend/src/test/java/edu/bbte/localproducersmarketplace/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bbte.localproducersmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
